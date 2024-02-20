4c624deec54107b0eebd2421af2d600a222c4ee6
ingrid-mo <morales.ingrid.i20@gmail.com>
2024-02-19 23:55:58 -0300

    agregue consusltas para hacer put y delete

--- consultas.js
+++ consultas.js
@@ -1,30 +1,55 @@
-import pkg from 'pg';
+import pkg from "pg";
 const { Pool } = pkg;
 
-
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'kataang65',
-    database: 'likeme',
-    allowExitOnIdle: true
+  host: "localhost",
+  user: "postgres",
+  password: "kataang65",
+  database: "likeme",
+  allowExitOnIdle: true,
 });
 const getDate = async () => {
-    const result = await pool.query("SELECT NOW()")
-    console.log(result)
-    }
-    
-const AgregarLike = async (titulo, img = 'image.jpg', descripcion, likes=0) => {
-    const consulta = "INSERT INTO posts VALUES (DEFAULT,$1, $2, $3, $4)";
-    const values = [titulo, img, descripcion, likes]; 
+  const result = await pool.query("SELECT NOW()");
+  console.log(result);
+};
+
+const AgregarLike = async (
+  titulo,
+  img = "image.jpg",
+  descripcion,
+  likes = 0
+) => {
+  const consulta = "INSERT INTO posts VALUES (DEFAULT,$1, $2, $3, $4)";
+  const values = [titulo, img, descripcion, likes];
+  try {
     const result = await pool.query(consulta, values);
-    console.log("posts agregado");
+    console.log("Post agregado");
+  } catch (err) {
+    console.error("Error al agregar el post", err);
+    throw err;
+  }
 };
 const obtenerlike = async () => {
-    const { rows } = await pool.query("SELECT * FROM posts")
-    console.log(rows)
-    return rows
-    }
+  const { rows } = await pool.query("SELECT * FROM posts");
+  console.log(rows);
+  return rows;
+};
 
+const EditPost = async (titulo, img, descripcion, likes,id ) => {
+  const consulta = "UPDATE posts SET titulo = $1, img = $2, descripcion = $3, likes = $4 WHERE id = $5"
+  const values = [titulo, img, descripcion, likes,id]
+  const { rowCount } = await pool.query(consulta, values)
+  
+  if (rowCount === 0) {
+  throw { code: 404, message: "No se consiguió ningún viaje con este id" }
+ 
+  }
+};
 
-export { obtenerlike, AgregarLike};
+const deletePost = async (id) => {
+  const consult = "DELETE FROM posts WHERE id = $1";
+  const values = [id];
+  const result = await pool.query(consult, values);
+};
+
+export { obtenerlike, AgregarLike, EditPost, deletePost };
--- index.js
+++ index.js
@@ -1,39 +1,71 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser'; 
-import { obtenerlike, AgregarLike } from './consultas.js';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { obtenerlike, AgregarLike, EditPost, deletePost  } from "./consultas.js";
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
-    console.log("Servidor encendido");
+  console.log("Servidor encendido");
 });
 
 app.get("/posts", async (req, res) => {
-    try {
-        const posts = await obtenerlike();
-        res.json(posts);
-    } catch (err) {
-        console.error('Error en la consulta', err);
-        res.status(500).json({ error: 'Error en el servidor' });
-    }
+  try {
+    const posts = await obtenerlike();
+    res.json(posts);
+  } catch (err) {
+    console.error("Error en la consulta", err);
+    res.status(500).json({ error: "Error en el servidor" });
+  }
 });
 app.get("/posts", async (req, res) => {
-    try {
-        const posts = await obtenerlike(); 
-        res.json(posts);
-    } catch (err) {
-        console.error('Error en la consulta', err);
-        res.status(500).json({ error: 'Error en el servidor' });
-    }
+  try {
+    const posts = await obtenerlike();
+    res.json(posts);
+  } catch (err) {
+    console.error("Error en la consulta", err);
+    res.status(500).json({ error: "Error en el servidor" });
+  }
 });
 
 app.post("/posts", async (req, res) => {
-    const { titulo, img, descripcion, likes } = req.body; 
-   await AgregarLike()
-    console.log(titulo, img, descripcion, likes); 
+  try {
+    const { titulo, img, descripcion, likes } = req.body;
+    await AgregarLike();
+    console.log(titulo, img, descripcion, likes);
     res.send("Nuevo post agregado");
+  } catch (err) {
+    console.error("Error en la consulta", err);
+    res.status(500).json({ error: "Error en el servidor" });
+  }
 });
+app.put("/posts/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { titulo, img, descripcion, likes } = req.query; 
+      await EditPost(titulo, img, descripcion, likes, id); 
+      res.send("Post modificado con éxito");
+    } catch (err) {
+      console.error("Error en la consulta", err);
+      if (err.code && err.message) {
+        res.status(err.code).json({ error: err.message });
+      } else {
+        res.status(500).json({ error: "Error en el servidor" });
+      }
+    }
+  });
 
+app.delete("/posts/:id", async (req, res) => {
+    try{
+        const { id } = req.params;
+        await deletePost(id);
+        res.send("Post eliminado con éxito");
+}
+    catch (err) {
+    console.error("Error en la consulta", err);
+    res.status(500).json({ error: "Error en el servidor" });
+  }
+
+});
